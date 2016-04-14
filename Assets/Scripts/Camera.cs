0cec0ba0287c04fd4f2a65a88e6a233ab53929d8
Kuan-Ting Chin <kt-chin@hotmail.com>
2016-04-15 06:59:33 +0800

    added repo

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    public Transform target;
+    private float speed = 10.0f;
+    private Vector3 point;
+    private Vector3 yAxis;
+	// Use this for initialization
+	void Start () {
+        point = target.transform.position;
+        transform.LookAt(point);
+        yAxis = new Vector3(0.0f,1.0f,0.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(point,yAxis,speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(point, yAxis,-speed * Time.deltaTime);
+        }
+	}
+}
